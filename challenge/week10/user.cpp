c0bf27d9f542ee6078230c9e6870043362937ed8
sxeony <https://github.com/sxeony/CPP202309.git>
2023-11-12 21:28:32 +0900

    week10 머드게임 개선

---
+++ challenge/week10/user.cpp
@@ -0,0 +1,5 @@
+#include "user.h"
+
+void User::DecreaseHP(int dec_hp) { hp = hp - dec_hp; }
+
+int User::GetHP() { return hp; }
---
+++ challenge/week10/user.h
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+class User {
+ public:
+  int hp;
+  void DecreaseHP(int dec_hp);
+  int GetHP();
+};
